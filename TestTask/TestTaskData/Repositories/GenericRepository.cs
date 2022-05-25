e26a475aee8887bf6e1e3beeb16bc35690733805
Max <rendon4ik@gmail.com>
2022-05-25 14:45:56 +0300

    Add Generic Repository

---
+++ TestTask/TestTaskData/Repositories/GenericRepository.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using TestTaskData.DbContexts;
+
+namespace TestTaskData.Repositories
+{
+    public class GenericRepository<TEntity> where TEntity : class
+    {
+        protected AppDbContext Context { get; }
+
+        protected DbSet<TEntity> DbSet { get; }
+
+        public GenericRepository(AppDbContext context)
+        {
+            Context = context;
+            DbSet = context.Set<TEntity>();
+        }
+
+        public virtual IEnumerable<TEntity> Get(
+            Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null)
+        {
+            IQueryable<TEntity> query = DbSet;
+
+            if (filter is not null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (orderBy is not null)
+            {
+                return orderBy(query).ToList();
+            }
+
+            return query.ToList();
+        }
+
+        public virtual TEntity GetById(string id)
+        {
+            return DbSet.Find(id);
+        }
+
+        public virtual TEntity Insert(TEntity entity)
+        {
+            return DbSet.Add(entity).Entity;
+        }
+
+        public virtual TEntity Update(TEntity entity)
+        {
+            return DbSet.Update(entity).Entity;
+        }
+
+        public virtual TEntity Delete(string id)
+        {
+            TEntity entity = GetById(id);
+            return DbSet.Remove(entity).Entity;
+        }
+    }
+}
